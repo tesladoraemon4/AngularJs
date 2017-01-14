9e5b42d9b468aaff9533a1582ae8253c728a9880
tesladoraemon4 <tesladoraemon5@gmail.com>
2017-01-13 18:40:03 -0600

    tutoria3_controlador

---
+++ controller.js
@@ -0,0 +1,15 @@
+var app = angular.module("MyFirstApp",[]);//NOmbre del modulo, modulos a importar 
+//retorna una referencia a la funcion 
+app.controller("FirstController",function ($scope) {
+	$scope.nombre="david";
+});
+
+//el objeto scope enlaza la informacion del driver con la vista 
+
+
+
+
+
+
+
+
--- tut2.php
+++ tut2.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
-<html  ng-app>
+<html  ng-app="MyFirstApp">
 <head>
 	<title></title>
      <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.8/angular.min.js"></script> 
      <script type="text/javascript" src="controller.js"></script>
 </head>
-<body>
+<body ng-controller="FirstController"><!---controller define el controlador-->
 
-{{"dshfjsahgf"}}
+{{"nombre: "+nombre}}
 
 </body>
 </html>

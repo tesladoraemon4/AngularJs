a7181cebfb4b294751c4023544bc8768421b8054
tesladoraemon4 <tesladoraemon5@gmail.com>
2017-01-14 13:58:59 -0600

    Vid6 inyeccion dependencias (Cambiar $scope por x)

--- controllercap6.js
+++ controllercap6.js
@@ -1,10 +1,43 @@
 var app = angular.module("MyFirstApp",[]);//NOmbre del modulo, 
 //modulos a importar 
 //retorna una referencia a la funcion 
-app.controller("FirstController",function ($scope) {
-//el objeto scope enlaza la informacion del driver con la vista 
+
+
+/*
+	*Cambios En la inyeccion de dependencias
+	Es cuando cambiamos los nombres de las variables con el fin 
+	de hacer mas pequeño el	JS
+	app.controller("NOmbre controlador",["cadena del objeto",function(var){
+		//codigo
+	}]);
+
+*/
+
+app.controller("FirstController",["$scope",function (s) {
+	$scope.nuevoComentario = {};
+
 	$scope.nombre="david";
-});
+
+	$scope.comentarios = [
+		{
+			comentario:"Comentarios c",
+			username:"David"
+		},
+		{
+			comentario:"Comentarios x",
+			username:"David 12343"
+		}
+
+	];
+
+
+	$scope.agregarComentario = function () {
+		$scope.comentarios.push($scope.nuevoComentario);
+		$scope.nuevoComentario ={};
+	}
+
+}])
+
 
 
 
--- tut2cap6.php
+++ tut2cap6.php
@@ -7,9 +7,42 @@
 </head>
 <body ng-controller="FirstController"><!---controller define el controlador-->
 
+
+<input type="text" ng-model="nuevoComentario.comentario">
+
+<input type="text" ng-model="nuevoComentario.username">
+
+<button ng-click="agregarComentario()">agregar comentario</button>
+
+
+
+
+
+
+<h3>COmentarios</h3>
+
+<ul>
+	<li ng-repeat= "comentario in comentarios">
+		{{comentario.comentario}}
+
+		<strong>{{comentario.username}}</strong>
+	</li>
+</ul>
+
+
+
+
+
+<h1>Ejemplo inicial</h1>
+
+
+
 <input type="text" ng-model="nombre">
 
 {{"nombre: "+nombre}}
+
+<br>
+
 
 </body>
 </html>

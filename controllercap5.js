ffefbc07d1edbb51a1fe410048d2c535811f99bd
tesladoraemon4 <tesladoraemon5@gmail.com>
2017-01-14 13:14:51 -0600

    vid5 bindings (comunican el controlador y vista)

--- controllercap5.js
+++ controllercap5.js
@@ -3,7 +3,29 @@ var app = angular.module("MyFirstApp",[]);//NOmbre del modulo,
 //retorna una referencia a la funcion 
 app.controller("FirstController",function ($scope) {
 //el objeto scope enlaza la informacion del driver con la vista 
+	$scope.nuevoComentario = {};
+
 	$scope.nombre="david";
+
+	$scope.comentarios = [
+		{
+			comentario:"Comentarios c",
+			username:"David"
+		},
+		{
+			comentario:"Comentarios x",
+			username:"David 12343"
+		}
+
+	];
+
+
+	$scope.agregarComentario = function () {
+		$scope.comentarios.push($scope.nuevoComentario);
+		$scope.nuevoComentario ={};
+	}
+
+
 });
 
 

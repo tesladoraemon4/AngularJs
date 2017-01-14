e91cf3a72b1c81597511d36b7f99aa7dafecea33
tesladoraemon4 <tesladoraemon5@gmail.com>
2017-01-13 18:24:35 -0600

    tutoria1

---
+++ tut2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html  ng-app>
+<head>
+	<title></title>
+     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.8/angular.min.js"></script> 
+     <script type="text/javascript" src="controller.js"></script>
+</head>
+<body>
+
+{{"dshfjsahgf"}}
+
+</body>
+</html>
